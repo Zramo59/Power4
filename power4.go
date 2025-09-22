4ffd9234be7ef38bd2fc8c0626897419edae30c8
Zramo59 <matteo.marchiset@ynov.com>
2025-09-22 11:54:09 +0200

    création de la matrice power

--- power4.go
+++ power4.go
@@ -11,33 +11,9 @@ func NewPower() *Power {
 	p := &Power{}
 	for i := 0; i < 6; i++ {
 		for j := 0; j < 7; j++ {
-			p.Table[i][j] = " "
+			p.Table[i][j] = ""
 		}
+		fmt.Println(p.Table)
 	}
-	fmt.Println(p.Table)
 	return p
 }
-
-// func (p *Power) Display() {
-// 	fmt.Println(" 0 1 2 3 4 5 6")
-// 	for i := 0; i < 6; i++ {
-// 		for j := 0; j < 7; j++ {
-// 			fmt.Print("|" + p.Table[i][j])
-// 		}
-// 		fmt.Println("|")
-// 	}
-// 	fmt.Println("---------------")
-// }
-
-// func (p *Power) PlaceToken(column int, token string) bool {
-// 	if column < 0 || column > 6 {
-// 		return false
-// 	}
-// 	for i := 5; i >= 0; i-- {
-// 		if p.Table[i][column] == " " {
-// 			p.Table[i][column] = token
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
