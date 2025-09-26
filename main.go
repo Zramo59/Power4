0365e9d08f71a01379d15d07759e7244d2a385ad
Zramo59 <matteo.marchiset@ynov.com>
2025-09-26 14:34:48 +0200

    Add HTML template and CSS styles for Power4 game interface

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 )
 
+var tmpl = template.Must(template.ParseFiles("index.html"))
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl.Execute(w, nil)
+}
+
 func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Serveur démarré sur http://localhost:8080")
+	//
+	http.ListenAndServe(":8080", nil)
 	p := NewPower()
 	Starter(p.Player)
 	p.PrintBoard()
